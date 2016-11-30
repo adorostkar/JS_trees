2e6c590bbe4c654c18cc282421bf1d8e6f538ceb
Ali Dorostkar <ashkan.dorostkar@gmail.com>
2016-11-30 22:22:50 +0100

    Added click and double click to show description and open link. A few bug fixes

--- relation/tree.js
+++ relation/tree.js
@@ -1,4 +1,4 @@
-var boxWidth = 170,
+var minBoxWidth = 170,
     boxHeight = 20,
     gap = {
         width: 70,
@@ -20,7 +20,8 @@ var Nodes = [];
 var links = [];
 var numLvls = 0,
     topMarginLvl = [],
-    count = [];
+    count = [],
+    boxWidth = [];
 
 var diagonal = d3.svg.diagonal()
     .projection(function (d) {
@@ -28,6 +29,23 @@ var diagonal = d3.svg.diagonal()
         return [d.y, d.x];
     });
 
+var nodeDescription;
+var tip = d3.tip()
+    .attr('class', 'd3-tip')
+//    .direction('e')
+    .offset([-10, 0])
+    .html(function(d) {
+        var str = "";
+        if (d.url){
+            str = "<font color=\"red\">Double Click to open course page</font><br/><br/>";
+        }
+        if(d.dsc)
+            str += nodeDescription;
+            
+        return str;
+    })
+
+// Find the node with the specified text as it name
 function find(text) {
     "use strict";
     var i;
@@ -59,20 +77,20 @@ d3.selection.prototype.moveToBack = function() {
 };
 
 function switch_state (d, stat) {
-    if (stat){
+//    if (stat){
         d3.select("#" + d.id).moveToFront().classed("activelink", stat); // change link color
         d3.select("#" + d.id).moveToFront().classed("link", !stat); // change link color
-    }else{
-        d3.select("#" + d.id).moveToBack().classed("activelink", stat); // change link color
-        d3.select("#" + d.id).moveToBack().classed("link", !stat); // change link color
-    }
+//    }else{
+//        d3.select("#" + d.id).moveToBack().classed("activelink", stat); // change link color
+//        d3.select("#" + d.id).moveToBack().classed("link", !stat); // change link color
+//    }
 }
 
 function mouse_action(val, stat, direction) {
     "use strict";
     d3.select("#" + val.id).classed("active", stat);
-    if(val.url)
-        d3.select("#" + val.id).classed("witurl", !stat);
+//    if(val.url)
+//        d3.select("#" + val.id).classed("witurl", !stat);
 
     links.forEach(function (d) {
         // This is used to reduce the amount of repetition in the code. 
@@ -102,28 +120,16 @@ function mouse_action(val, stat, direction) {
     });
 }
 
-var tip = d3.tip()
-    .attr('class', 'd3-tip')
-//    .direction('e')
-    .offset([-10, 0])
-    .html(function(d) {
-        var str = "";
-        if (d.url && d.dsc){
-            str = "<font color=\"red\">Click to open website</font><br/><br/>";
-            str += d.dsc;
-        }else if(d.url)
-            str = "<font color=\"red\">Click to open website</font>";
-        else
-            str = d.dsc;
-            
-        return str;
-    })
-
 function renderRelationshipGraph(data) {
     "use strict";
 
+    // Compute the placement of each node based on its level and the size
+    // of previous levels.
     data.Nodes.forEach(function (d, i) {
-        d.x = margin.left + d.lvl * (boxWidth + gap.width);
+        var bv = 0;
+        for (var j = 0; j < d.lvl; j++)
+            bv += boxWidth[j] + gap.width;
+        d.x = margin.left + bv;//d.lvl * (boxWidth + gap.width);
         d.y = topMarginLvl[d.lvl] + (boxHeight + gap.height) * count[d.lvl];
         d.id = "n" + i;
         count[d.lvl] += 1;
@@ -131,11 +137,16 @@ function renderRelationshipGraph(data) {
     });
 
     data.links.forEach(function (d) {
-        links.push({
-            source: find(d.source),
-            target: find(d.target),
-            id: "l" + find(d.source).id + find(d.target).id
-        });
+        // If there is no node with the mentioned id, omit the link
+        if( find(d.source) && find(d.target)){
+            links.push({
+                source: find(d.source),
+                target: find(d.target),
+                id: "l" + find(d.source).id + find(d.target).id
+            });
+        }else{
+            console.warn("One of the IDs \'" +d.source+ "\' or \'" + d.target + "\' do not point to a node");
+        }
     });
 
     svg.append("g")
@@ -152,22 +163,32 @@ function renderRelationshipGraph(data) {
         .attr("x", function (d) { return d.x; })
         .attr("y", function (d) { return d.y; })
         .attr("id", function (d) { return d.id; })
-        .attr("width", boxWidth)
+        .attr("width", function (d) { return boxWidth[d.lvl];})
         .attr("height", boxHeight)
-        .attr("class", function (d) { return d.url? "node witurl":"node";})
+        .attr("class", function (d) { return "node";})
         .attr("rx", 6)
         .attr("ry", 6)
         .on("mouseover", function () {
             var d = d3.select(this).datum();
             mouse_action(d, true, "root");
-            if(d.url || d.dsc) tip.show(d)
+            // D O N T   R E M O V E   T H I S
+            // It is refered to in a comment.
+            if(d.url || d.dsc) { 
+                nodeDescription = "Click to see a short description";
+                tip.show(d)
+            }
         })
         .on("mouseout", function () {
             var d = d3.select(this).datum();
             mouse_action(d, false, "root");
-            if(d.url || d.dsc) tip.hide(d)
+            tip.hide();
         })
-        .on("click", function(d) { if(d.url) window.open(d.url); }); // If a url is available, put a click event
+//        .on("click", function(d) { if(d.url) window.open(d.url); }); // If a url is available, put a click event
+        // Use this if you wish the tooltip to show when the box is clicked 
+        // and open the url when the box is double clicked.
+        // Otherwise comment them and uncomment the line above this comment and the line in "mouseover" event.
+        .on("click", function(d) { if(d.url || d.dsc) { nodeDescription = d.dsc; tip.show(d);} }) // If a url is available, put a click event
+        .on("dblclick", function(d) { if(d.url) window.open(d.url); }); // If a url is available, put a click event
 
     node.append("text")
         .attr("class", "label")
@@ -175,6 +196,7 @@ function renderRelationshipGraph(data) {
         .attr("y", function (d) { return d.y + 15; })
         .text(function (d) { return d.text; });
 
+    // Create the links between the boxes
     links.forEach(function (li) {
         svg.append("path", "g")
             .attr("class", "link")
@@ -190,9 +212,9 @@ function renderRelationshipGraph(data) {
                 };
                 
                 if (oSource.y < oTarget.y) {
-                    oSource.y += boxWidth;
+                    oSource.y += boxWidth[li.source.lvl];
                 } else {
-                    oTarget.y += boxWidth;
+                    oTarget.y += boxWidth[li.target.lvl];
                 }
                 return diagonal({
                     source: oSource,
@@ -202,29 +224,66 @@ function renderRelationshipGraph(data) {
     });
 }
 
+// This function computes the size of the box needed to fit the node text.
+window.getWidthOfText = function(txt){
+  var classes = document.styleSheets[0].rules || document.styleSheets[0].cssRules;
+    for (var x = 0; x < classes.length; x++) {
+        if (classes[x].selectorText === ".label") {
+            // Create dummy span
+            this.e = document.createElement('span');
+            // Set font-size
+            this.e.className = "label";
+            // Set text
+            this.e.innerHTML = txt;
+            document.body.appendChild(this.e);
+            // Get width NOW, since the dummy span is about to be removed from the document
+            var w = this.e.offsetWidth + 20;
+            // Cleanup
+            document.body.removeChild(this.e);
+            // All right, we're done
+            return w;
+        }
+    }
+}
+
 // M A I N   C O D E
 
 d3.json("flare.json", function (json) {
     "use strict";
     data = json;
     
+    // Initialize the array variables
     data.Nodes.forEach(function (d) {
         count[d.lvl] = 0;
         topMarginLvl[d.lvl] = 0;
+        boxWidth[d.lvl] = minBoxWidth;
     });
     numLvls = count.length;
     
+    // Find the number of items in each level and the width of each level.
     data.Nodes.forEach(function (d) {
         topMarginLvl[d.lvl] += 1;
+        if(getWidthOfText(d.text) > boxWidth[d.lvl])
+            boxWidth[d.lvl] = getWidthOfText(d.text);
     });
     
+    // Compute the total width of the page.
+    width = 0;
+    for(var i = 0; i < boxWidth.length-1; i++){
+        width += boxWidth[i] + gap.width;
+    }
+    
+    width += margin.left + margin.right + boxWidth[boxWidth.length-1];
+    
+    // Which level is has the most elements. Used to compute the height of the page
     var largest = Math.max.apply(Math, topMarginLvl);
     
     // Update height and width to have all the nodes in the image and also have 
     // the smallest image possible.
     height = margin.top + margin.bottom + largest*boxHeight + (largest - 1) * gap.height;
-    width  = margin.left + margin.right + topMarginLvl.length*boxWidth + (topMarginLvl.length - 1)*gap.width;
+//    width  = margin.left + margin.right + topMarginLvl.length*boxWidth + (topMarginLvl.length - 1)*gap.width;
     
+    // Find the top margin of each column(Level).
     for (var i = 0; i < topMarginLvl.length; i++){
         topMarginLvl[i] = (height - topMarginLvl[i]*boxHeight - (topMarginLvl[i]-1)*gap.height)/2;
     }
